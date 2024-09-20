993cd5d71c655bd0834a16592691f8c41a936784
seanhuangg <sean.huang@torontomu.ca>
2024-09-19 20:04:01 -0400

    Changed profile picture

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -12,9 +12,9 @@ const Photo = () => {
                 animate={{ opacity: 1, transition: { delay: 0.4, duration: 0.2, ease: "easeInOut" } }}
                 className="flex justify-center items-center"
             >
-                <div className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten">
+                <div className="relative w-[198px] h-[198px] xl:w-[398px] xl:h-[398px] mix-blend-lighten">
                     <Image
-                        src="/assets/work/photo.png"
+                        src="/assets/work/portrait.png"
                         priority
                         quality={100}
                         fill
@@ -22,6 +22,7 @@ const Photo = () => {
                         className="object-contain"
                     />
                 </div>
+
             </motion.div>
         </div>
     )
